8a5c02b45bb95db9798d4c453d511b962060ca14
Student <example@example.com>
2019-10-30 15:02:24 -0700

    move old code to scripts2.js, refactor code in scripts.js to use RegEx

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,25 @@
 // Business Logic
 function latinizer(word) {
   var pigWord = word;
-  if (/[^a-z]/i.test(pigWord)) {
+  var aeiou = /[aeiou]/i;
+  var aeiouy = /[aeiouy]/i;
+
+  var sliceLength = 0;
+  if (word.includes("qu")) {
+    var sliceLength = 1;
+  }
+
+  if (/[^a-z]/i.test(word)) {
     return pigWord;
   }
-  else if (word.length === 1) {
-    pigWord += "ay";
-  }
-  else if ((word.charAt(0).toLowerCase() === "a") || (word.charAt(0).toLowerCase() === "e") || (word.charAt(0).toLowerCase() === "i") || (word.charAt(0).toLowerCase() === "o") || (word.charAt(0).toLowerCase() === "u")) {
+  else if ((word.length === 1) || (word.search(aeiou) === 0)) {
     pigWord += "way";
   }
-  else if (!word.includes("qu")) {
-    var firstVowelIndex = vowelIndexer(word);
-    var pigWord = word.slice(firstVowelIndex) + word.slice(0, firstVowelIndex) + "ay";
-  }
   else {
-    var firstVowelIndex = vowelIndexer(word);
-    var pigWord = word.slice(firstVowelIndex+1) + word.slice(0, firstVowelIndex+1) + "ay";
+    var firstVowelIndex = word.search(aeiouy);
+    var pigWord = word.slice(firstVowelIndex + sliceLength) + word.slice(0, firstVowelIndex + sliceLength) + "ay";
   }
   return pigWord;
-}
-
-function vowelIndexer(string) {
-  for (var i = 1; i < string.length; i++) {
-    if ((string.charAt(i).toLowerCase() === "a") || (string.charAt(i).toLowerCase() === "e") || (string.charAt(i).toLowerCase() === "i") || (string.charAt(i).toLowerCase() === "o") || (string.charAt(i).toLowerCase() === "u") || (string.charAt(i).toLowerCase() === "y")) {
-      return i;
-    }
-  }
 }
 
 function toPigLatin(sentence) {
@@ -37,7 +30,6 @@ function toPigLatin(sentence) {
   var pigSentence = pigSentenceArray.join(" ");
   return pigSentence;
 }
-
 
 
 // User interface Logic
---
+++ js/scripts2.js
@@ -0,0 +1,53 @@
+// Business Logic
+function latinizer(word) {
+  var pigWord = word;
+  if (/[^a-z]/i.test(word)) {
+    return pigWord;
+  }
+  else if (word.length === 1) {
+    pigWord += "ay";
+  }
+  else if ((word.charAt(0).toLowerCase() === "a") || (word.charAt(0).toLowerCase() === "e") || (word.charAt(0).toLowerCase() === "i") || (word.charAt(0).toLowerCase() === "o") || (word.charAt(0).toLowerCase() === "u")) {
+    pigWord += "way";
+  }
+  else if (!word.includes("qu")) {
+    var firstVowelIndex = vowelIndexer(word);
+    var pigWord = word.slice(firstVowelIndex) + word.slice(0, firstVowelIndex) + "ay";
+  }
+  else {
+    var firstVowelIndex = vowelIndexer(word);
+    var pigWord = word.slice(firstVowelIndex+1) + word.slice(0, firstVowelIndex+1) + "ay";
+  }
+  return pigWord;
+}
+
+function vowelIndexer(string) {
+  for (var i = 1; i < string.length; i++) {
+    if ((string.charAt(i).toLowerCase() === "a") || (string.charAt(i).toLowerCase() === "e") || (string.charAt(i).toLowerCase() === "i") || (string.charAt(i).toLowerCase() === "o") || (string.charAt(i).toLowerCase() === "u") || (string.charAt(i).toLowerCase() === "y")) {
+      return i;
+    }
+  }
+}
+
+function toPigLatin(sentence) {
+  var sentenceArray = sentence.split(" ");
+  var pigSentenceArray = sentenceArray.map(function(word) {
+    return latinizer(word);
+  });
+  var pigSentence = pigSentenceArray.join(" ");
+  return pigSentence;
+}
+
+
+
+// User interface Logic
+$(document).ready(function() {
+  $("#entry").submit(function(event) {
+    event.preventDefault();
+
+    $("div#output").show();
+    var userSentence = $("input#userSentence").val();
+    var pigLatinSentence = toPigLatin(userSentence);
+    $("p#sentence").text(pigLatinSentence);
+  });
+});
