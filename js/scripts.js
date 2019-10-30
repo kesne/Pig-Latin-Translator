7d1065b33feecf930b49fed94869b480484a9576
Student <example@example.com>
2019-10-30 12:25:14 -0700

    extend toPigLatin function, add function to locate index of first vowel

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,25 @@ function toPigLatin(sentence) {
   else if (sentence.length === 1) {
     pigSentence += "ay";
   }
-
+  else if ((sentence.charAt(0).toLowerCase() === "a") || (sentence.charAt(0).toLowerCase() === "e") || (sentence.charAt(0).toLowerCase() === "i") || (sentence.charAt(0).toLowerCase() === "o") || (sentence.charAt(0).toLowerCase() === "u")) {
+    pigSentence += "way";
+  }
+  else if (!sentence.includes("qu")) {
+    var firstVowelIndex = vowelIndexer(sentence);
+    console.log("Got it.");
+    console.log("Index is", firstVowelIndex);
+  }
 
   return pigSentence;
 }
 
-
+function vowelIndexer(string) {
+  for (var i = 1; i < string.length; i++) {
+    if ((string.charAt(i).toLowerCase() === "a") || (string.charAt(i).toLowerCase() === "e") || (string.charAt(i).toLowerCase() === "i") || (string.charAt(i).toLowerCase() === "o") || (string.charAt(i).toLowerCase() === "u")) {
+      return i;
+    }
+  }
+}
 
 
 
